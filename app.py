3b7734cc512e954826956b913ccfba1da936c0b6
sob0994 <69644179+sob0994@users.noreply.github.com>
2022-01-20 08:35:02 +0800

    Update to deplopy

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from app import app
 
 if __name__ == "__main__":
-    app.run(host="192.168.1.101", port=5000, debug=True)
-    # app.run()
+    # app.run(host="192.168.1.101", port=5000, debug=True)
+    app.run()
